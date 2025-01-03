c2549cdbdb54aede88cae88f1c8e670a48ce5c65
avoytovich <levanwork@gmail.com>
2025-01-03 23:05:59 +0100

    add first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
 'use client';
 
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, Suspense, lazy } from 'react';
+import { useRouter } from 'next/navigation';
 
 import AddNewGroup from '@/components/AddNewGroup';
-import GroupSelect from '@/components/GroupSelect';
-import NavigateButton from '@/components/NavigateButton';
+import LoadingIndicator from '@/components/common/LoadingIndicator';
 import { useServices } from '@/services';
-import { handleError } from '@/utils';
+import { handleNavigate, handleError } from '@/utils';
 
-const GroupSelector: React.FC = () => {
+const Shelf = lazy(() => import('@/components/Shelf'));
+
+const Library: React.FC = () => {
   const [groups, setGroups] = useState<string[]>([]);
-  const [selectedGroup, setSelectedGroup] = useState<string>('');
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
 
   const { fetchGroups } = useServices();
 
   useEffect(() => {
     const fetchGroupsList = async () => {
-      setLoading(true);
       try {
         const data = await fetchGroups();
         setGroups(data);
@@ -31,27 +33,30 @@ const GroupSelector: React.FC = () => {
     fetchGroupsList();
   }, [fetchGroups]);
 
-  const handleGroupSelect = useCallback((group: string) => {
-    setSelectedGroup(group);
-  }, []);
+  const handleGroupSelect = useCallback(
+    (group: string) => {
+      handleNavigate(router, `/documents?group=${group}`);
+    },
+    [router]
+  );
 
   return (
     <div className="min-h-screen bg-gray-100 p-6 flex justify-center items-center">
-      <div className="max-w-md w-full bg-white p-6 rounded shadow">
+      <div className="max-w-xl w-full bg-white p-6 rounded shadow">
         <div className="flex justify-between items-center mb-4">
-          <h1 className="text-xl font-bold mb-4">Select a Group</h1>
-          <AddNewGroup groups={groups} />
+          <h1 className="text-xl font-bold mb-4">Library</h1>
+          <AddNewGroup groups={groups} isFetchingGroups={loading} />
         </div>
-        <GroupSelect
-          groups={groups}
-          selectedGroup={selectedGroup}
-          handleGroupSelect={handleGroupSelect}
-          loading={loading}
-        />
-        <NavigateButton selectedGroup={selectedGroup} loading={loading} />
+        {loading ? (
+          <LoadingIndicator />
+        ) : (
+          <Suspense fallback={<LoadingIndicator />}>
+            <Shelf groups={groups} handleGroupSelect={handleGroupSelect} />
+          </Suspense>
+        )}
       </div>
     </div>
   );
 };
 
-export default GroupSelector;
+export default Library;
